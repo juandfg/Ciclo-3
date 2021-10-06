f70d89400d13ddbd7a18339114e7bff3dfc1ef27
juandfg <juandfg_92@hotmail.com>
2021-10-06 09:20:47 -0500

    aspecto visual

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bcryptjs = require('bcryptjs');
 const connection = require('../datbase/db');
 const router = express.Router();
+const calcular = require('../calcular/calcular');
 
 router.get("/", (req, res) =>{
     res.render("index.html");
@@ -12,7 +13,7 @@ router.get("/login.html", (req, res) =>{
 });
 router.post('/auth', async (req, res) =>{
     const user = req.body.user;
-    const pass = req.body.user;
+    const pass = req.body.pass;
     let passwordHaash = await bcryptjs.hash(pass, 8);
     if(user && pass){
         connection.query('SELECT * FROM users WHERE user = ?', [user], async (error, results) =>{
@@ -23,7 +24,8 @@ router.post('/auth', async (req, res) =>{
                     alertMessage: "¡USUARIO O PASSWORD INCORRECTO!",
                     alertIcon: 'error',
                     showConfirmButton: false,
-                    timer: 1500,
+                    timer: 2000,
+                    ruta:'login.html'
                 })
             }else{
                 res.render('login.html',{
@@ -33,7 +35,7 @@ router.post('/auth', async (req, res) =>{
                     alertIcon: 'success',
                     showConfirmButton: false,
                     timer: 1500,
-                    ruta: ''
+                    ruta: '/'
                 })    
             }
         });
@@ -45,23 +47,40 @@ router.get("/registro.html", (req, res) =>{
 });
 router.post('/registro.html', async (req, res) =>{
     const user = req.body.user;
-    const pass = req.body.user;
+    const pass = req.body.pass;
     let passwordHaash = await bcryptjs.hash(pass, 8);
-        connection.query('INSERT INTO users SET ?',{user:user, pass:passwordHaash}, async (error, results) =>{
-            if(error){
-                console.log(error)
+    if(user && pass){
+        connection.query('SELECT * FROM users WHERE user = ?', [user], async (error, results) =>{
+            if(results.length == 0){
+                
+                connection.query('INSERT INTO users SET ?',{user:user, pass:passwordHaash}, async (error, results) =>{
+                    if(error){
+                        console.log(error)
+                    }else{
+                        res.render('registro.html',{
+                        alert:true,
+                        alertTitle: "Registration",
+                        alertMessage: "¡Successful Registration!",
+                        alertIcon: 'success',
+                        showConfirmButton: false,
+                        timer: 1500,
+                        ruta: 'login.html'
+                        })
+                    }
+                });
             }else{
                 res.render('registro.html',{
-                alert:true,
-                alertTitle: "Registration",
-                alertMessage: "¡Successful Registration!",
-                alertIcon: 'success',
-                showConfirmButton: false,
-                timer: 1500,
-                ruta: ''
-            })
-        }
-    });
+                    alert:true,
+                    alertTitle: "Oops...",
+                    alertMessage: "¡USUARIO YA EN USO!",
+                    alertIcon: 'error',
+                    showConfirmButton: false,
+                    timer: 2000,
+                    ruta:'registro.html'
+                });
+            }
+        });
+    }
 });
 
 
@@ -69,8 +88,79 @@ router.get("/contact.html", (req, res) =>{
     res.render("contact.html");
 });
 
+//productos
 router.get("/productos.html", (req, res) =>{
     res.render("productos.html");
+});
+router.post('/productos.html', (req, res)=>{
+    const CantHeta5 = req.body.CantHetaPalma;
+    const CantHeta1= req.body.CantHetaMaiz;
+    const CantHeta2= req.body.CantHetaCafe;
+    const CantHeta3= req.body.CantHetaArroz;
+    const CantHeta4= req.body.CantHetaCacao;
+    if(CantHeta5 > 0){
+        let palce = 51313100;
+        res.render('productos.html',{
+            alert:true,
+            alertTitle: "Hecho",
+            alertMessage: "¡Total!"+ calcular.operacion(CantHeta5,palce),
+            alertIcon: 'success',
+            showConfirmButton: false,
+            footer: '<a href="/pagar.html">Desea agregar al carrito?</a>',
+            timer: '',
+            ruta: 'productos.html'
+            });
+        }if(CantHeta1 > 0){
+            let palce = 3568000;
+            res.render('productos.html',{
+                alert:true,
+                alertTitle: "Hecho",
+                alertMessage: "¡Total!"+ calcular.operacion(CantHeta1,palce),
+                alertIcon: 'success',
+                showConfirmButton: false,
+                footer: '<a href="/pagar.html">Desea agregar al carrito?</a>',
+                timer: '',
+                ruta: 'productos.html'
+                });
+        }else if(CantHeta2 > 0){
+            let palce = 2226440;
+            res.render('productos.html',{
+                alert:true,
+                alertTitle: "Hecho",
+                alertMessage: "¡Total!"+ calcular.operacion(CantHeta2,palce),
+                alertIcon: 'success',
+                showConfirmButton: false,
+                footer: '<a href="/pagar.html">Desea agregar al carrito?</a>',
+                timer: '',
+                ruta: 'productos.html'
+                });
+        }else if(CantHeta3 > 0){
+            let palce = 1275950;
+            res.render('productos.html',{
+                alert:true,
+                alertTitle: "Hecho",
+                alertMessage: "¡Total!"+ calcular.operacion(CantHeta3,palce),
+                alertIcon: 'success',
+                showConfirmButton: false,
+                footer: '<a href="/pagar.html">Desea agregar al carrito?</a>',
+                timer: '',
+                ruta: 'productos.html'
+                });
+        }else if(CantHeta4 > 0){
+            let palce = 1144400;
+            res.render('productos.html',{
+                alert:true,
+                alertTitle: "Hecho",
+                alertMessage: "¡Total!"+ calcular.operacion(CantHeta4,palce),
+                alertIcon: 'success',
+                showConfirmButton: false,
+                footer: '<a href="/pagar.html">Desea agregar al carrito?</a>',
+                timer: '',
+                ruta: 'productos.html'
+                });
+        }else{
+            console.log('bobo hp')
+        }
 });
 
 router.get("/pagar.html", (req, res) =>{
