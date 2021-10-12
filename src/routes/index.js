6ee339cd4c2f63bb786e63255462a206fba32eac
juandfg <juandfg_92@hotmail.com>
2021-10-12 18:10:18 -0500

    jwt agregado

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,9 +3,14 @@ const bcryptjs = require('bcryptjs');
 const connection = require('../datbase/db');
 const router = express.Router();
 const calcular = require('../calcular/calcular');
+const jwt = require('jsonwebtoken');
+const {promisify} = require('util');
+const cookieParser = require('cookie-parser');
+//const authcontroller = require('../controller/authcontroller')
 
-router.get("/", (req, res) =>{
-    res.render("index.html");
+
+router.get('/', /*isAuthenticateduser,*/ (req, res) =>{
+    res.render('index.html', {user: req.user});
 });
 
 router.get("/login.html", (req, res) =>{
@@ -28,6 +33,17 @@ router.post('/auth', async (req, res) =>{
                     ruta:'login.html'
                 })
             }else{
+                const id = results[0].idusers;
+                const token = jwt.sign({id:id}, process.env.JWT_SECRETO,{
+                    expiresIn:process.env.JWT_TIEMPO_EXPIRA
+                
+                });
+                console.log(token)
+                const cookiesOptions = {
+                    expires: new Date(Date.now()+process.env.JWT_COOKIE_EXPIRES*24*60*60*1000),
+                    httpOnly:true
+                }
+                res.cookie('jwt', token, cookiesOptions)
                 res.render('login.html',{
                     alert:true,
                     alertTitle: "login correcto",
@@ -42,18 +58,68 @@ router.post('/auth', async (req, res) =>{
     }
 });
 
+async function isAuthenticated(req, res, next){
+    if(req.cookies.jwt){
+        try {
+            const decodificada = await promisify(jwt.verify)(req.cookies.jwt, process.env.JWT_SECRETO)
+            connection.query('SELECT * FROM users WHERE idusers = ?', [decodificada.id], (error, results)=>{
+            if(decodificada.id != 33){res.render('login.html')}
+            req.user = results[0]
+            return next()
+        })
+        } catch (error) {
+            console.log(error)
+            return next()
+        }
+    }else{
+        res.redirect('/')
+        
+    }
+};
+async function isAuthenticateduser(req, res, next){
+    if(req.cookies.jwt){
+        try {
+            const decodificada = await promisify(jwt.verify)(req.cookies.jwt, process.env.JWT_SECRETO)
+            connection.query('SELECT * FROM users WHERE idusers = ?', [decodificada.id], (error, results)=>{
+            if(!results){res.render('/')}
+            req.user = results[0]
+            return next()
+        })
+        } catch (error) {
+            console.log(error)
+            return next()
+        }
+        
+    }else{
+        res.redirect('login.html')
+        
+    }
+    
+};
+
+router.get('/logout', (req, res)=>{
+    res.clearCookie('jwt')
+    return res.redirect('/')
+});
+
+//router.post('/auth', authcontroller.login)
+
+
 router.get("/registro.html", (req, res) =>{
     res.render("registro.html");
 });
 router.post('/registro.html', async (req, res) =>{
     const user = req.body.user;
     const pass = req.body.pass;
+    const apellido = req.body.apellido;
+    const email = req.body.email;
+    const direccion = req.body.direccion;
     let passwordHaash = await bcryptjs.hash(pass, 8);
     if(user && pass){
         connection.query('SELECT * FROM users WHERE user = ?', [user], async (error, results) =>{
             if(results.length == 0){
                 
-                connection.query('INSERT INTO users SET ?',{user:user, pass:passwordHaash}, async (error, results) =>{
+                connection.query('INSERT INTO users SET ?',{user:user, apellido:apellido, email:email, dirección:direccion, password:passwordHaash}, async (error, results) =>{
                     if(error){
                         console.log(error)
                     }else{
@@ -163,8 +229,15 @@ router.post('/productos.html', (req, res)=>{
         }
 });
 
-router.get("/pagar.html", (req, res) =>{
+//cart
+router.get("/pagar.html",isAuthenticateduser, (req, res) =>{
     res.render("pagar.html");
 });
+
+//admin
+router.get("/administrador.html", isAuthenticated, (req, res)=>{
+    res.render("administrador.html");
+});
+
 
 module.exports = router;
