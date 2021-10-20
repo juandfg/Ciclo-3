695be4eb1e778d657d13d2ad94aa52259164324d
juandfg <juandfg_92@hotmail.com>
2021-10-20 18:46:17 -0500

    prueba

--- src/datbase/db.js
+++ src/datbase/db.js
@@ -6,7 +6,6 @@ const connection = mysql.createConnection({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
-    port: process.env.DB_PORT
 });
 
 connection.connect((error)=>{
